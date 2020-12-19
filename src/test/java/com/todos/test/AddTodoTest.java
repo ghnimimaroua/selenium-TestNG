8eeb2b1fb545d4724ab29771b954f6fc292bc9ec
ghnimi.maroua <ghnimi.maroua@gmail.com>
2020-12-19 14:46:34 +0100

    Selenieum with TestNG

---
+++ src/test/java/com/todos/test/AddTodoTest.java
@@ -0,0 +1,30 @@
+package com.todos.test;
+
+import java.io.IOException;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.todos.pages.TodoPage;
+import com.todos.utils.Setup;
+
+public class AddTodoTest extends Setup {
+
+	public AddTodoTest() throws IOException {
+		super ();
+	}
+	
+	TodoPage todoPage;
+	
+	@Test
+	public void iCanAddTodo() throws IOException {
+		todoPage = new TodoPage();
+		boolean elementText = todoPage.isElementDisplayed(TodoPage.inputText);
+		Assert.assertTrue(elementText);
+		todoPage.submitTodo(prop.getProperty("todo1"));
+		String elementTodo = todoPage.checkElementContains(TodoPage.element1);
+		Assert.assertTrue(elementTodo.contains(prop.getProperty("todo1")));
+		boolean checkbox = todoPage.isCheckboxSelected(todoPage.checkbox);
+		Assert.assertFalse(checkbox);
+	}
+}
